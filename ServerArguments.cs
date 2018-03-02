66f05984eb1aa3e883358bd661f1c78df97ae08c
lowgear <lowgear1000@gmail.com>
2018-03-02 14:14:40 +0500

    Fixed negative delay problem. Added screenshots.

--- ServerArguments.cs
+++ ServerArguments.cs
@@ -7,13 +7,21 @@ namespace SNTP_server
     {
         public int SecondsDelay { get; set; }
 
+        public bool NegativeDelay { get; set; }
+
         public static bool TryParse(string[] args, out ServerArguments serverArguments, TextWriter logWriter)
         {
             var argumentsParser = new FluentCommandLineParser<ServerArguments>();
 
             argumentsParser.Setup(a => a.SecondsDelay)
                 .As('d', "delay")
-                .SetDefault(0);
+                .SetDefault(0)
+                .WithDescription("In seconds");
+
+            argumentsParser.Setup(a => a.NegativeDelay)
+                .As('n', "negative")
+                .SetDefault(false)
+                .WithDescription("Use for negatove delay");
 
             argumentsParser.SetupHelp("?", "h", "help")
                 .Callback(text => logWriter.WriteLine(text));
@@ -26,6 +34,9 @@ namespace SNTP_server
                 serverArguments = null;
                 return false;
             }
+
+            if (argumentsParser.Object.NegativeDelay)
+                argumentsParser.Object.SecondsDelay *= -1;
 
             serverArguments = argumentsParser.Object;
             return !parsedResult.HelpCalled;
